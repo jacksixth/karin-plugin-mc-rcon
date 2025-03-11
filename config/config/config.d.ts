01a85254aefaa919ce10dd9f76705aa14afb6480
jack <>
2025-03-11 16:00:36 +0800

    fix(config): 为服务器配置添加别名功能
    - 在 Config 接口中添加了 alias 字段，用于服务器别名
    - 在 web.config.ts 中增加了服务器别名的输入组件

--- config/config/config.d.ts
+++ config/config/config.d.ts
@@ -1,5 +1,6 @@
 export interface Config {
   servers: {
+    alias: string //服务器别名
     host: string //服务器host
     port: number //服务器端口 默认25565
     rconPort: number //服务器rcon端口 默认25575
--- src/web.config.ts
+++ src/web.config.ts
@@ -8,40 +8,50 @@ export default {
   },
   /** 动态渲染的组件 */
   components: () => [
-    components.accordionPro.create("servers", defConfig.servers, {
-      label: "服务器列表",
-      description: "配置你的服务器列表",
-      children: components.accordion.createItem("servers-item", {
-        title: "服务器",
-        children: [
-          components.input.string("host", {
-            label: "服务器host",
-            defaultValue: "localhost",
-            rules: [
-              {
-                regex:
-                  /^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$|^([0-9a-zA-Z-]{1,}\.)+([a-zA-Z]{2,})$/,
-                error: "请输入正确的IP地址或域名",
-              },
-            ],
-          }),
-          components.input.number("port", {
-            label: "服务器端口",
-            defaultValue: "25565",
-            rules: [{ min: 1, max: 65535 }],
-          }),
-          components.input.number("rconPort", {
-            label: "服务器rcon端口",
-            defaultValue: "25575",
-            rules: [{ min: 1, max: 65535 }],
-          }),
-          components.input.string("password", {
-            label: "rcon密码",
-            defaultValue: "password",
-          }),
-        ],
+    components.accordionPro.create(
+      "servers",
+      defConfig.servers.map((item, index) => {
+        return {
+          ...item,
+          title: item.alias,
+          subtitle: `${item.host}:${item.port}`,
+        }
       }),
-    }),
+      {
+        label: "服务器列表",
+        description: "配置你的服务器列表",
+        children: components.accordion.createItem("servers-item", {
+          title: "服务器",
+          subtitle: "配置你的服务器",
+          children: [
+            components.input.string("alias", {
+              label: "服务器别名",
+            }),
+            components.input.string("host", {
+              label: "服务器host",
+              rules: [
+                {
+                  regex:
+                    /^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$|^([0-9a-zA-Z-]{1,}\.)+([a-zA-Z]{2,})$/,
+                  error: "请输入正确的IP地址或域名",
+                },
+              ],
+            }),
+            components.input.number("port", {
+              label: "服务器端口",
+              rules: [{ min: 1, max: 65535 }],
+            }),
+            components.input.number("rconPort", {
+              label: "服务器rcon端口",
+              rules: [{ min: 1, max: 65535 }],
+            }),
+            components.input.string("password", {
+              label: "rcon密码",
+            }),
+          ],
+        }),
+      }
+    ),
   ],
 
   /** 前端点击保存之后调用的方法 */
