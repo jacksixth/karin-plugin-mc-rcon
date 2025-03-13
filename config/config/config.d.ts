a7535831da740345d35cdec40a74c9a881444fee
jack <>
2025-03-13 17:41:26 +0800

    feat(config): 封禁玩家功能-待实现
    - 实现了部分功能
    - 在 config.json 中添加 banQQ 数组，用于存储封禁的 QQ 号
    - 在 config.d.ts 中添加 banQQ 类型定义
    - 更新 README.md，添加 #ban 命令说明
    
    此更新增加了封禁玩家功能，管理员可以使用 #ban 命令封禁玩家，使其无法使用机器人功能。

--- config/config/config.d.ts
+++ config/config/config.d.ts
@@ -5,9 +5,11 @@ export interface Config {
     port: string //服务器端口 一般是25565
     rconPort: string //服务器rcon端口 一般是25575
     password: string //服务器rcon密码 建议复杂点
-  }[],
-  QQNoLinkMcNickname: {
-    //QQ号与mc昵称的对应关系 用于查询玩家的mc昵称 
-    [qq:string]:string
   }[]
+  QQNoLinkMcNickname: {
+    //QQ号与mc昵称的对应关系 用于查询玩家的mc昵称
+    qqNo: string
+    mcNickname: string
+  }[]
+  banQQ: string[] //封禁QQ号
 }
